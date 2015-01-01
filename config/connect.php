290fe05999ddbc662599b64a21ffbcad90aaf1f1
Southparkfan <Southparkfan@users.noreply.github.com>
2015-01-01 16:31:57 +0100

    Add dbhost + use __DIR__ for require

--- config/connect.php
+++ config/connect.php
@@ -1,7 +1,8 @@
 <?php
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/wikiportret/common/meekrodb.class.php';
+    require_once __DIR__ . '/../common/meekrodb.class.php';
 
     DB::$user = ''; // database username
     DB::$password = ''; // database password
     DB::$dbName = ''; // database name
+    DB::$host = 'localhost'; // database server
 ?>
